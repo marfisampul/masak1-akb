f05d1a9309330a08b68cc7a447fd46cc10d2e012
marfisampul <marfisampul@gmail.com>
2022-08-13 02:07:16 +0700

    first commit

--- app/src/main/java/com/example/masakapah/MainActivity.java
+++ app/src/main/java/com/example/masakapah/MainActivity.java
@@ -3,34 +3,14 @@ package com.example.masakapah;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.DefaultItemAnimator;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Toast;
 
-import com.example.masakapah.Adapter.MasakAdapter;
-import com.example.masakapah.Api.ApiEndPoint;
-import com.example.masakapah.Api.ApiService;
 import com.example.masakapah.Fragment.FragmentCategory;
 import com.example.masakapah.Fragment.FragmentFav;
 import com.example.masakapah.Fragment.FragmentHome;
-import com.example.masakapah.Fragment.FragmentSetting;
-import com.example.masakapah.Model.ModelMasak;
-import com.example.masakapah.Model.ResponseModel;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -66,9 +46,6 @@ public class MainActivity extends AppCompatActivity {
                         fragment = new FragmentFav();
                         break;
 
-                    case R.id.setting:
-                        fragment = new FragmentSetting();
-                        break;
                 }
                 return getFragmentPage(fragment);
             }
